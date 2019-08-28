f852cfaf563a3fed2a24b1fbd400fdb60cb7d2f4
khanhtungmtp <umlatao97@gmail.com>
2019-08-28 22:43:25 +0700

    create factory seeder laravel

---
+++ database/factories/Channel.php
@@ -0,0 +1,20 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Channel;
+use App\Model;
+use App\User;
+use Faker\Generator as Faker;
+
+$factory->define(Channel::class, function (Faker $faker)
+{
+    return [
+        'name'    => $faker->sentence(3),
+        'user_id' => function ()
+        {
+            return factory(User::class)->create()->id;
+        },
+        'description' => $faker->sentence(30)
+    ];
+});
